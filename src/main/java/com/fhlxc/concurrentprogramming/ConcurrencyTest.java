7357407b45e148a044761db411c0d1ce2fe13459
fhlxc <z15881614642@163.com>
2020-04-05 17:35:50 +0800

    第一章

---
+++ src/main/java/com/fhlxc/concurrentprogramming/ConcurrencyTest.java
@@ -0,0 +1,67 @@
+package com.fhlxc.concurrentprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+* @author Xingchao Long
+* @date 2020年4月3日 下午5:49:59
+* @classname ConcurrencyTest
+* @description 并发与串行执行时间的比较
+* 1亿 快一倍
+* 1千万 慢
+* 1百万 慢
+* 10万 慢
+* 1万 慢
+* 原因: 上下文切换和线程创建需要时间
+*/
+
+public class ConcurrencyTest {
+
+    public static long count;
+    
+    private static void concurrency() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        Thread thread = new Thread(new Runnable() {
+            
+            @Override
+            public void run() {
+                @SuppressWarnings("unused")
+                int a = 0;
+                for (long i = 0; i < count; i++) {
+                    a += 5;
+                }
+            }
+        });
+        thread.start();
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        thread.join();
+        System.out.println("concurrency: " + time + "ms, b = " + b);
+    }
+    
+    private static void serial() {
+        long start = System.currentTimeMillis();
+        int a = 0;
+        for (long i = 0; i < count; i++) {
+            a += 5;
+        }
+        int b = 0; 
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("serial: " + time + "ms, b = " + b + ", a = " + a);
+    }
+    
+    public static void main(String[] args) throws NumberFormatException, IOException, InterruptedException {
+        count = Long.parseLong(new BufferedReader(new InputStreamReader(System.in)).readLine());
+        concurrency();
+        serial();
+    }
+
+}
---
+++ src/main/java/com/fhlxc/concurrentprogramming/DeadLock.java
@@ -0,0 +1,52 @@
+package com.fhlxc.concurrentprogramming;
+
+/**
+* @author Xingchao Long
+* @date 2020年4月3日 下午8:44:22
+* @classname DeadLock
+* @description 
+*/
+
+public class DeadLock {
+
+    private static String A = "A";
+    private static String B = "B";
+    
+    public static void main(String[] args) {
+        new DeadLock().deadLock();
+    }
+    
+    public void deadLock() {
+        Thread t1 = new Thread(new Runnable() {
+            
+            @Override
+            public void run() {
+                synchronized (A) {
+                    try {
+                        Thread.currentThread();
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (B) {
+                        System.out.println("1");
+                    }
+                }
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            
+            @Override
+            public void run() {
+                synchronized (B) {
+                    synchronized (A) {
+                        System.out.println("2");
+                    }
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+    }
+
+}
