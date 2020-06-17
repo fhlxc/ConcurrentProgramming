8dc901de60413268f1f48f09a8fd962e2e2ee067
lxc <foreverhappylxc@163.com>
2020-06-17 17:41:21 +0800

    换系统

---
+++ src/test/java/com/fhlxc/concurrentprogramming/Test.java
@@ -0,0 +1,12 @@
+package com.fhlxc.concurrentprogramming;
+
+/**
+* @author Xingchao Long
+* @date 2020年6月14日 下午2:07:38
+* @classname Test
+* @description
+*/
+
+public interface Test {
+    public void test();
+}
---
+++ src/test/java/com/fhlxc/concurrentprogramming/Test1.java
@@ -0,0 +1,16 @@
+package com.fhlxc.concurrentprogramming;
+
+/**
+* @author Xingchao Long
+* @date 2020年6月14日 下午2:08:46
+* @classname Test1
+* @description
+*/
+
+public class Test1 implements Test {
+
+    @Override
+    public void test() {
+    }
+
+}
